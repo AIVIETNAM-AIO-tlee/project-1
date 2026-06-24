0f9c0a3b4c4edf6dffed9ed9861a35ff65c1e8d2
AIVIETNAM-AIO-tlee <quangthanh17022004@gmail.com>
2026-06-25 05:56:18 +0700

    chore: add download VnCoreNLP

--- helper/nlp_t2s.py
+++ helper/nlp_t2s.py
@@ -7,7 +7,8 @@ import streamlit as st
 # Cấu hình môi trường cho Java, tùy mng set java
 os.environ["JAVA_HOME"] = r"C:\Program Files\Java\jre-1.8"
 
-py_vncorenlp.download_model(save_dir='VnCoreNLP/models')
+if not os.path.exists("VnCoreNLP/models"):
+    py_vncorenlp.download_model(save_dir="VnCoreNLP")
 
 BASE_DIR = os.path.dirname(os.path.abspath("project-1-1/"))
 VNCORENLP_DIR = os.path.join(BASE_DIR, "VnCoreNLP")
